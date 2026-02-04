d2a5892efe81ad23000b0b9f009c4533ba9b265b
rat.in.machine <rat.in.machine@proton.me>
2026-02-04 14:12:55 +0100

    feat: steamapi devuelve estructuras de datos

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from structures.game import SteamGame, PriceOverview, Genre, Platforms
-import requests
+from utils.steam_requests import SteamAPI
+
 
 def main():
-    appid = 570  
-    url = f"https://store.steampowered.com/api/appdetails?appids={appid}&cc=us&l=en"
-    response = requests.get(url).json()
-
-    data = response[str(appid)]["data"]
-
-    game = SteamGame(
-        appid=data["steam_appid"],
-        name=data["name"],
-        short_description=data.get("short_description", ""),
-        developers=data.get("developers", []),
-        publishers=data.get("publishers", []),
-        price_overview=PriceOverview(**data["price_overview"]) if "price_overview" in data else None,
-        platforms=Platforms(**data["platforms"]),
-        genres=[Genre(**g) for g in data.get("genres", [])],
-        is_free=data.get("is_free", False)
-    )
-
+    steam = SteamAPI()
+    game = steam.get_game_by_appid(570)
     print(game)
+
 
 if __name__ == "__main__":
     main()
-    
